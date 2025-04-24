74c630ae4d3b7c0d6d2cbda03fdbd783e2742619
Ahmed-Qotb <bl7ax99fg@gmail.com>
2025-04-24 23:03:44 +0200

    third commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 const authPages = new Set(["/sign-in", "/sign-up", "/forget-password"]);
-const privatePages = new Set(["/dashboard", "/quiz-history"]);
+const privatePages = new Set(["/dashboard", "/quiz-history" , "/"]);
 
 export default async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
